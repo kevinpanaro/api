099ce4761809ff42d4515c3afb29dc57f815d25c
kevinpanaro <panaro.kevin@gmail.com>
2018-01-29 02:37:43 -0500

    initial monks

---
+++ beer/breweries/monks.py
@@ -0,0 +1,10 @@
+'''
+Author: Kevin Panaro
+Date: 1.28.15
+Purpose: Grabs Evil Genius beers
+
+Technically not a brewery, and I want to limit this api to just
+craft brewery places that change their taps a lot, but Monk's
+kinda falls under the category because their beer selection is 
+continuous.
+'''
