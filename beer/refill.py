5dc2130c72f071941d5c7a6b8bc80c39f6e32880
kevinpanaro <panaro.kevin@gmail.com>
2018-01-29 02:18:06 -0500

    main initial commit

---
+++ beer/refill.py
@@ -0,0 +1,10 @@
+from breweries.evilgenius import evil_genius
+from breweries.tiredhands import tired_hands
+
+
+def pour_beer():
+    evil_genius()
+    tired_hands()
+
+if __name__ == '__main__':
+    pour_beer()
