80e2c337a6519efac606ea011a23f0a7a64d19fa
kevinpanaro <panaro.kevin@gmail.com>
2018-01-29 20:07:20 -0500

    change info

--- beer/breweries/monks.py
+++ beer/breweries/monks.py
@@ -1,14 +1,13 @@
 '''
 Author: Kevin Panaro
 Date: 1.28.15
-Purpose: Grabs Evil Genius beers
+Purpose: Grabs Monks beers on tap
 
 Technically not a brewery, and I want to limit this api to just
 craft brewery places that change their taps a lot, but Monk's
 kinda falls under the category because their beer selection is 
 continuous.
 
-TODO: everything
 '''
 import logging
 import re
