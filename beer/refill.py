7eee1df60783442f36b92b5e5e9a66f8d156b83d
kevinpanaro <panaro.kevin@gmail.com>
2018-01-30 14:21:59 -0500

    update refill to include dock street

--- beer/refill.py
+++ beer/refill.py
@@ -3,12 +3,14 @@ Used basically as a main
 '''
 from breweries.evilgenius import evil_genius
 from breweries.tiredhands import tired_hands
+from breweries.monks import monks
 
 
 def pour_beer():
     '''pour beer into the taps, heh...'''
     evil_genius()
     tired_hands()
+    monks()
 
 if __name__ == '__main__':
     pour_beer()
