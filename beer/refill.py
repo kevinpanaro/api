8ada6784a925882f813274717696b471349980ba
kevinpanaro <panaro.kevin@gmail.com>
2018-03-21 15:19:57 -0600

    add equinox, remove monks (later fix)

--- beer/refill.py
+++ beer/refill.py
@@ -3,14 +3,18 @@ Used basically as a main
 '''
 from breweries.evilgenius import evil_genius
 from breweries.tiredhands import tired_hands
+from breweries.dockstreet import dock_street
 from breweries.monks import monks
+from breweries.equinox import equinox
 
 
 def pour_beer():
     '''pour beer into the taps, heh...'''
     evil_genius()
     tired_hands()
-    monks()
+    dock_street()
+    equinox()
+    # monks() # a later thing
 
 if __name__ == '__main__':
     pour_beer()
