a30eb8ae13dd6625029fe90005becbfc81cb1a25
kevinpanaro <panaro.kevin@gmail.com>
2018-03-26 18:24:31 -0600

    add cookie support for beautiful_url

--- beer/breweries/helpers/url_pull.py
+++ beer/breweries/helpers/url_pull.py
@@ -1,10 +1,23 @@
 from bs4 import BeautifulSoup as bs
-from requests import get
+from requests import get, Request, Session, cookies
 from contextlib import closing
 
-def beautiful_url(url):
+def beautiful_url(url, cookie=False):
     '''simple url grab but also closes nicely and beautiful soups it'''
     try:
+        if cookie:
+            name, content, domain, path = cookie
+            jar = cookies.RequestsCookieJar()
+            jar.set(name, content, domain=domain, path=path) 
+            req = Request('GET', url, cookies=jar)
+            req = req.prepare()
+            s = Session()
+            r = s.send(req)
+            if is_good_response(r):
+                souped_url = bs(r.text, "html.parser")
+                return souped_url
+            else:
+                return None
         with closing(get(url)) as resp:
             if is_good_response(resp):
                 souped_url = bs(resp.text, "html.parser")
