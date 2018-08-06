e73212731c01a58fae145ef3b2a0c6359da2a304
kevinpanaro <panaro.kevin@gmail.com>
2018-08-06 13:33:29 -0400

    beer: breweries: helpers: base brewery class initial commit

---
+++ beer/breweries/helpers/breweries.py
@@ -0,0 +1,11 @@
+
+class Brewery():
+
+    def get_name(self):
+        return(self.brewery)
+
+    def get_base_url(self):
+        return(self.base_url)
+
+    def get_save_file(self):
+        return(self.save_file)
