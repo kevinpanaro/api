7fefd1b07e01157cba6af8a5a853d8cf2455035f
kevinpanaro <panaro.kevin@gmail.com>
2018-08-08 18:41:15 -0400

    beer: breweries: (helpers:) add __init__

--- beer/breweries/__init__.py
+++ beer/breweries/__init__.py
@@ -1,3 +1,21 @@
-'''
+"""
 TODO: Fix formatting and define a set json
-'''
+"""
+
+from .dockstreet import dock_street
+from .equinox import equinox
+from .evilgenius import evil_genius
+from .mayorofoldtown import mayorofoldtown
+from .monks import monks
+from .odell import odell
+from .tiredhands import tired_hands
+
+__all__ = [
+           "dock_street",
+           "equinox",
+           "evil_genius",
+           "mayorofoldtown",
+           "monks",
+           "odell",
+           "tired_hands",
+           ]
---
+++ beer/breweries/helpers/__init__.py
@@ -0,0 +1,7 @@
+from .url_pull import beautiful_url
+from .save_beer import save_beer
+
+__all__ = [
+           "beautiful_url", 
+           "save_beer",
+           ]
