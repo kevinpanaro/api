625c5370fee81625bbe601429db08b9952db9b2c
kevinpanaro <panaro.kevin@gmail.com>
2018-08-10 02:06:04 -0400

    beer: breweries: helpers: add b_id to __all__

--- beer/breweries/helpers/__init__.py
+++ beer/breweries/helpers/__init__.py
@@ -1,11 +1,13 @@
 from .helpers import (
     beautiful_url,
     save_beer,
-    format_beer_dict
+    format_beer_dict,
+    b_id,
     )
 
 __all__ = [
            "beautiful_url", 
            "save_beer",
            "format_beer_dict",
+           "b_id",
            ]
