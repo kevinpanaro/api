f7ecc13236902a83224ae5154601b9ce66c7d331
kevinpanaro <panaro.kevin@gmail.com>
2018-08-14 13:39:34 -0400

    beer: breweries: odell cookie, javascript fix

--- beer/breweries/odell.py
+++ beer/breweries/odell.py
@@ -3,7 +3,12 @@ Author:  Kevin Panaro
 Date:    3.21.18
 Purpose: Grabs Odell Beers on tap
 
-TODO: Fix this cookie bullshit
+What happened?
+
+My cookie's were working but the actual beers were 
+rendere by a javascript script. So now i'm using
+requests-html, to render the website, before 
+collecting the raw_html
 '''
 import logging
 
@@ -15,68 +20,93 @@ except:
 BASE_URL = "https://www.odellbrewing.com/taproom{}/"
 BREWERY = "Odell Brewing" 
 SAVE_FILE = "odell.json"
-COOKIE = {'name': 'odAccess', 'value': 'true', 'domain': 'www.odellbrewing.com', 'path': '/'}
+COOKIES = [{'name': 'odAccess', 'value': 'true', 'domain': 'www.odellbrewing.com', 'path': '/'}]
  
 locations = {"Fort Collins": "", "Denver": "-denver"}
 
-def get_beers_url(url):
-    html = beautiful_url(url, COOKIE)
-    # logging.info(html)
-    all_beers = html.find_all('div', {"class": "item-bg-color menu-item"})
+# def get_beers_url(url):
+#     html = beautiful_url(url=url, cookies=COOKIES, javascript=True)
+#     all_beers = html.find_all('div', {"class": "item-bg-color menu-item"})
     
-    beer_urls = []
-    for url in html.find_all('p', {'class':'tap-beer'}):
-        beer_urls.append(url.find('a')['href'])
-    logging.debug("{} beer urls found".format(len(beer_urls)))
-    return(beer_urls)
+#     beer_urls = []
+#     for url in html.find_all('p', {'class':'tap-beer'}):
+#         beer_urls.append(url.find('a')['href'])
+#     logging.debug("{} beer urls found".format(len(beer_urls)))
+#     return(beer_urls)
 
 def parse_url(url):
-
-    beer_urls = get_beers_url(url)
+    beers = beautiful_url(url=url, cookies=COOKIES, javascript=True)
+    beers = beers.find_all('div', {'class': 'item-bg-color menu-item'})
+    logging.info(f"Found {len(beers)} beers")
     return_beers = []
 
-    for beer_url in beer_urls:
+    _id = get_id("beer_id")
+
+    for beer in beers:
+        print(beer)
         beer_dict = {}
-        beer = beautiful_url(beer_url, COOKIE)
+        beer_name = None
+        beer_description = None
+        beer_notes = None
+        beer_stats = {}
 
-        beer_name = beer.find('div', {'class':'columns small-12 beer-title'}).get_text().strip()
+        beer_brewery = BREWERY # everything served here is by Tired Hands
+        beer_abv = None
+        beer_ibu = None
+        beer_hops = []
+        beer_malts = []
+        beer_avail = []
+        beer_style = None
+        beer_dict = {}
 
-        logging.debug("Beer Found: {}".format(beer_name))
+        beer_name = beer.find('span', {'class':'item'}).get_text().strip()
+        logging.info(f"Beer Found: {beer_name}")
 
-        beer_description = beer.find('div', {'class':'beer-content'}).get_text()
+        beer_brewery = beer.find('span', {"class": "brewery-name-hideable brewery hide"}).get_text()
+        logging.info(f"Brewery Found: {beer_brewery}")
 
-        if beer_description:
-            logging.debug("Description: Found")
-        else:
-            logging.warning("No Description Found")
+        beer_style = beer.find('span', {'class':'beer-style-hideable beer-style'}).get_text()
+        logging.info(f"Style Found: {beer_style}")
 
-        beer_details = beer.find_all('div', {'class':'columns small-12 medium-12'})
+        beer_abv = beer.find('span', {'class': 'abv-hideable abv'}).get_text()
+        logging.info(f"ABV Found: {beer_abv}")
 
-        beer_details = ([detail.get_text().strip() for detail in beer_details])
+        try:
+            beer_ibu = beer.find('span', {"class": "ibu-hideable ibu hide"}).get_text()
+            logging.info(f"IBU Found: {beer_ibu}")
+        except:
+            pass
 
-        ibu, style, abv, availability = beer_details
-
-        beer_stats = {'ibu': ibu.split(':')[-1],
-                      'style': style,
-                      'abv': abv.split(':')[-1],
-                      'availability': availability}
-
-        beer_dict = {"beer": beer_name,
-                     "description": beer_description,
-                     "stats": beer_stats}   
+        beer_description = beer.find('p', {'class': 'show-less item-title-color'}).get_text()
+        
+        beer_dict = format_beer_dict(_id              = _id,
+                                     _type            = "beer",
+                                     beer_name        = beer_name,
+                                     beer_description = beer_description,
+                                     beer_brewery     = beer_brewery,
+                                     beer_abv         = beer_abv,
+                                     beer_ibu         = beer_ibu,
+                                     beer_hops        = beer_hops,
+                                     beer_malts       = beer_malts,
+                                     beer_avail       = beer_avail,
+                                     beer_style       = beer_style,)  
 
         return_beers.append(beer_dict)
+        _id += 1
+
+    set_id(file_name = "beer_id", starting_id = _id)
+
     return(return_beers)   
 
 def odell():
 
-    logLevel=logging.DEBUG
+    logLevel=logging.INFO
     FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(message)s'
     logging.basicConfig(format=FORMAT,level=logLevel)
 
     try:
         output = []
-        # cookie = ('odAccess', 'true', 'www.odellbrewing.com', '/')
+
         for location, url in locations.items():
             logging.info("Location: {}".format(location))
             beers = parse_url(BASE_URL.format(url))
@@ -85,7 +115,7 @@ def odell():
         output = {"locations": output, "establishment": BREWERY, "id": b_id()[BREWERY], "type": "establishment"}
         save_beer(output, SAVE_FILE)
         
-        print("{} completed".format(BREWERY))
+        logging.info(f"Complete: {BREWERY}")
     except Exception as e:
         logging.warning(f"{e} failed.")
 
