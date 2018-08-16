d385a82123871ebba7fe69cf5f48f657ecc166cf
kevinpanaro <panaro.kevin@gmail.com>
2018-08-16 12:11:44 -0500

    beer: breweries: helpers: working single page

--- beer/breweries/helpers/breweries.py
+++ beer/breweries/helpers/breweries.py
@@ -2,33 +2,35 @@ import helpers
 
 class Brewery():
 
-    def __init__(self, brewery: str, base_url: str, cookies: list, javascript: bool, save_file: str,
+    def __init__(self, brewery_name: str, base_url: str, cookies: list, javascript: bool, save_file: str,
                  locations: dict, single_page: bool, beers_html_tags,
-                 beer_name_tags, beer_description_tags, beer_abv_tags = None,
+                 beer_name_tags, beer_description_tags, beer_brewery_tags = None,beer_abv_tags = None,
                  beer_ibu_tags = None, beer_hops_tags = [], beer_malts_tags = [],
                  beer_avail_tags = [], beer_style_tags = None, beer_multi_page_tags = None, kill = []):
-        self.brewery = brewery
-        self.base_url = base_url
-        self.cookies = cookies
-        self.javascript = javascript
-        self.save_file = save_file
-        self.locations = locations
-        self.single_page = single_page
-        self.beers_html_tags = beers_html_tags
-        self.beer_name_tags = beer_name_tags
-        self.beer_description_tags = beer_description_tags
-        self.beer_abv_tags = beer_abv_tags
-        self.beer_ibu_tags = beer_ibu_tags
-        self.beer_hops_tags = beer_hops_tags
-        self.beer_malts_tags = beer_malts_tags
-        self.beer_avail_tags = beer_avail_tags
-        self.beer_style_tags = beer_style_tags
-        self.beer_multi_page_tags = beer_multi_page_tags
-        self.kill = kill
+
+        self.brewery_name = brewery_name            # name of the brewery
+        self.base_url = base_url                    # base url for the brewery
+        self.cookies = cookies                      # list of cookies
+        self.javascript = javascript                # bool determining if javascript is needed
+        self.save_file = save_file                  # end save file name
+        self.locations = locations                  # list of brewery locations
+        self.single_page = single_page              # bool whether all beers are on the same page
+        self.beers_html_tags = beers_html_tags      # parent html tag that encompasses all beers on a html site
+        self.beer_name_tags = beer_name_tags        # html tags for the name of the beer
+        self.beer_description_tags = beer_description_tags # html tags for the description of the beer
+        self.beer_brewery_tags = beer_brewery_tags  # html tags if location has more than just their beer on tap
+        self.beer_abv_tags = beer_abv_tags          # html tags for the abv of the beer
+        self.beer_ibu_tags = beer_ibu_tags          # html tags for the ibu of the beer
+        self.beer_hops_tags = beer_hops_tags        # html tags for the hops of the beer
+        self.beer_malts_tags = beer_malts_tags      # html tags for the malts of the beer
+        self.beer_avail_tags = beer_avail_tags      # html tags for the availability of the beer
+        self.beer_style_tags = beer_style_tags      # html tags for the style of the beer
+        self.beer_multi_page_tags = beer_multi_page_tags    # html tags for urls of the beers if not single_page
+        self.kill = kill                            # kill if these are found
 
         self.beer_name = None
         self.beer_description = None
-        self.beer_brewery = self.brewery
+        self.beer_brewery = self.brewery_name
         self.beer_abv = None
         self.beer_ibu = None
         self.beer_hops = []
@@ -59,8 +61,8 @@ class Brewery():
         helpers.set_id(file_name = "location_id", starting_id = _id)
 
         output = {"locations": output, 
-                  "establishment": self.brewery, 
-                  "id": helpers.b_id()[self.brewery], 
+                  "establishment": self.brewery_name, 
+                  "id": helpers.b_id()[self.brewery_name], 
                   "type": "establishment"}
 
         helpers.save_beer(output, self.save_file)
@@ -70,10 +72,22 @@ class Brewery():
 
         _id = helpers.get_id("beer_id")
         for beer in self.beers:
-            if self.single_page:
+            """
+            At this point it is required that beer
+            either be, or become a bs4 object.
+            self.beers will be a list of bs4 objects,
+            or a list of urls to specific beer pages. 
+            """
+
+            #
+            if helpers.valid_url(beer):
+                beer = helpers.beautiful_url(url=beer, 
+                                     cookies=self.cookies, 
+                                     javascript=self.javascript)
+
+            if self.single_page: # will get rid off
                 try:
-                    self.beer_name = self.get_beer_info(beer, self.beer_name_tags)
-                    self.beer_description = self.get_beer_info(beer, self.beer_description_tags)
+                    self.set_all_beer_info(beer)
 
                     beer_dict = helpers.format_beer_dict(_id              = _id,
                                                          _type            = "beer",
@@ -117,3 +131,32 @@ class Brewery():
     def get_beer_info(self, beer, tags):
         tag, attribute = tags
         return(beer.find(tag, attribute).get_text().strip())
+
+    def set_all_beer_info(self, beer):
+        """ 
+        This attempts to set all the beer info from the tags provided
+        
+        :param beer: a specific beers bs4 object
+        :returns: all beer info (by setting by reference (?))
+        """
+        def try_to_set(beer_value, beer_value_tags):
+            """
+            value is an beer part, eg. name, brewery, abv...
+            """
+            try:
+                beer_value = self.get_beer_info(beer, beer_value_tags)
+            except:
+                pass
+            return(beer_value)
+
+        self.beer_name = try_to_set(self.beer_name, self.beer_name_tags)
+        self.beer_description = try_to_set(self.beer_description, self.beer_description_tags)
+        self.beer_brewery = try_to_set(self.beer_brewery, self.beer_brewery_tags)
+        self.beer_abv = try_to_set(self.beer_abv, self.beer_abv_tags)
+        self.beer_ibu = try_to_set(self.beer_ibu, self.beer_ibu_tags)
+        self.beer_hops = try_to_set(self.beer_hops, self.beer_hops_tags)
+        self.beer_malts = try_to_set(self.beer_malts, self.beer_malts_tags)
+        self.beer_avail = try_to_set(self.beer_avail, self.beer_avail_tags)
+        self.beer_style = try_to_set(self.beer_style, self.beer_style_tags)
+
+
